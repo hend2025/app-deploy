c1de8bb1af63082eb518c91b4ba3490dd8a6adda
hend <heniandong@a-eye.cn>
2025-11-05 17:18:36 +0800

    1

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    name: 'root',
+    component: () => import('../views/VerMgt.vue')
+  },
+  {
     path: '/deploy/',
     name: 'VerMgt',
     component: () => import('../views/VerMgt.vue')
